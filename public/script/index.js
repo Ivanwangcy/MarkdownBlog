e4fa193d89436497a6febeacf2a1b4ee736b9f0b
Ivan <wcy5211678@163.com>
2016-03-26 21:28:48 +0800

    add react

---
+++ public/script/index.js
@@ -0,0 +1,10 @@
+var CommentBox = React.createClass({
+    render: function() {
+      return <div>
+        <h1>Note</h1>
+      </div>;
+    }
+});
+
+
+ReactDOM.render(<CommentBox/>, document.getElementById("content"));
--- server.js
+++ server.js
@@ -3,10 +3,10 @@ var path = require('path');
 var express = require('express');
 var app = express();
 
-app.set('port', (process.env.PORT || 4000));
+app.set('port', (process.env.PORT || 4001));
 app.use("/", express.static(path.join(__dirname, "public"))); // 默认路由
 
 
 app.listen(app.get('port'), function() {
-  console.log(('Server started: http://localhost:' + app.get('port')));
+  console.log(('Server started: http://localhost:' + app.get('port') + "/"));
 });
