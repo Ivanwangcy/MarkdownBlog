c99182115756887db63d10924ba635406e1e1af4
Ivan <wcy5211678@163.com>
2016-03-26 14:56:52 +0800

    server.js

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # MarkdownBlog
 This is markdown bolg project.   
 基于 React 开发， 后端基于 Experss。
-## To use
+## To use  
+```bash
+$ npm istall
+$ npm start
+```
--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 4000));
-app.use("/", express.static(path.join(__dirname, "public")))
+app.use("/", express.static(path.join(__dirname, "public"))); // 默认路由
 
 
 app.listen(app.get('port'), function() {
   console.log(('Server started: http://localhost:' + app.get('port')));
-})
+});
