4441b6d154b93be5a5c5d5f6e3a41d79584fa698
Ivan <wcy5211678@163.com>
2016-03-27 11:22:11 +0800

    notebox react component

--- public/script/index.js
+++ public/script/index.js
@@ -4,13 +4,31 @@ var NoteList = React.createClass({
   }
 });
 
+
+/**
+ * React 实例化组件不是真实的DOM节点
+ * 自定义组件以大写字母开头 -- while custom React component names begin with an uppercase letter.
+ */
 var NoteBox = React.createClass({
     render: function() {
-      return <div className="NoteBox">
-        <h1>Note</h1>
-      </div>;
+      return (
+        // JSX raw format -- React.createElement('div', {className: 'noteBox'}, 'Hello world!');
+        <div className="noteBox">
+          <h1>Ivan’s Note</h1>
+          Hello, world! I am a NoteBox.
+        </div>
+      );
     }
 });
 
 
-ReactDOM.render(<NoteBox/>, document.getElementById("content"));
+/**
+ * [render 渲染页面]
+ * @param  {[type]} [笔记框 NoteBox Component]
+ * @param  {[type]} [添加到Dom节点]
+ * @return {[type]} [none]
+ */
+ReactDOM.render(
+  <NoteBox />, // of raw React.createElement(NoteBox, null);
+  document.getElementById("content")
+);
